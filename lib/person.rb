637236acaff1f7910996e3621e688449c90e0228
Teerth Patel <teerth1205@gmail.com>
2020-02-06 20:42:52 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,6 +6,6 @@ attr_accessor  :name, :birthday, :hair_color, :eye_color, :height,
 
 def initialize(attributes)
   attributes.each do |key, value|
-    
+     self.send(("#{key}="), value)
 end 
 end
