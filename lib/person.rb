51f544fb920a1020f55e2b1ff302ea69b17f697f
Teerth Patel <teerth1205@gmail.com>
2020-02-06 20:43:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ attr_accessor  :name, :birthday, :hair_color, :eye_color, :height,
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-
+attributes = 
 def initialize(attributes)
   attributes.each do |key, value|
      self.send(("#{key}="), value)
